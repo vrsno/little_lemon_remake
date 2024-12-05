249c2179dafbb4ad660033179c31d16bac2b6d46
sinname <nexxuz2017@gmail.com>
2024-12-05 17:47:11 -0500

    header css ...

--- little-Lemon/src/App.jsx
+++ little-Lemon/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header'
 import {Nav} from './components/Nav'
 import {Main} from './components/Main'
--- little-Lemon/src/components/Header.jsx
+++ little-Lemon/src/components/Header.jsx
@@ -1,9 +1,17 @@
-
-
+import food1 from '../assets/food1.avif'
+import './header.css'
 export function Header(){
     return (
-        <header>
-            <h1>header</h1>
+        <header className="header">
+            <div className="header_container">
+                <h1>little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</p>
+                <button>Reserve a Table</button>
+            </div>
+            <div className="header_logo_container">
+            <img className='header_img' src={food1} alt="food" />
+            </div>
         </header>
     )
 }
